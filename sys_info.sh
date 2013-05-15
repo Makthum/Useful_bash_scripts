c9903cfca1b2094faae18f1317f3b7f0e9f7086d
makthum <makthum@gmail.com>
2013-05-15 09:46:23 -0700

    Adding files to repo

---
+++ sys_info.sh
@@ -0,0 +1,13 @@
+
+clear
+echo -e "\n"
+echo -e  "###############USERS LOGGED IN ##################"
+who
+echo -e "\n\n\n"
+echo "#################CURRENTLY LOGGEND IN AS#########"
+who am i
+echo -e "\n\n"
+echo "################# SYSTEM UPTIME################# "
+uptime
+echo -e "\n################CPU INFORMATION################"
+cat /proc/cpuinfo 
