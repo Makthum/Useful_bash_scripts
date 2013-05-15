bdf6db5b45c5afb524bd9ea66e1967b1f2f3a41a
makthum <makthum@gmail.com>
2013-05-15 16:52:11 -0700

    Updated sys_info

--- sys_info.sh
+++ sys_info.sh
@@ -6,25 +6,78 @@ echo -e " ######################################################################
 echo -e " \n \t              welcome to System Information Tool \n "
 echo    " ##########################################################################"
 until [ $selection -eq "9" ]; do
- 
+clear
+echo    " ##########################################################################"
 echo -e "\n \t                Select your input for More Info "
 echo -e "\n \t  		1: User Information "
 echo -e "\n \t 		2:  Login Information and Uptime "
 echo -e "\n \t  		3:  CPU Information "
 echo -e "\n \t  		4:  Display Information 
          \n \t 		5:  OS information 
-         \n \t  		6:  Network Configuration "
-echo -e "\n \t 		Enter your Input : \c" 
+         \n \t  		6:  Network Configuration 
+         \n \t                9:Exit"
+echo -e   " \n ##########################################################################"
+echo -e "\n \t 	           Enter your Input : \c" 
+
 read selection
+
+case $selection in
+
+1) clear 
+   echo -e "#########################################################################"
+   echo "                   Printing Display Resolution Information for this machine"
+   echo    "#########################################################################"
+   echo "  "
+   echo "  " 
+   who 
+      echo  -e  " \n#########################################################################"
+   echo  -e  "\n Press Any key to Continue to main menu ..........................."
+   read -n 1 key
+   ;;
+2) clear 
+   echo -e "#########################################################################"
+   echo "                   Printing Login information & Uptime  for this machine"
+   echo    "#########################################################################"
+   echo "  "
+   echo "  " 
+   echo "#################CURRENTLY LOGGEND IN AS#########"
+   who am i
+   echo -e "\n\n"
+   echo "################# SYSTEM UPTIME################# "
+   uptime 
+   echo  -e  "\n #########################################################################"
+   echo  -e  "\n Press Any key to Continue to main menu ..........................."
+   read -n 1 key
+   ;;
+
+3) clear 
+   echo -e "#########################################################################"
+   echo "                   Printing User Information for this machine"
+   echo    "#########################################################################"
+   echo "  "
+   echo "  " 
+   echo -e "\n################CPU INFORMATION################ /c"
+    cat /proc/cpuinfo | more 
+      echo -e   "\n #########################################################################"
+   echo  -e  "\n Press Any key to Continue to main menu ..........................."
+   read -n 1 key
+   ;;
+4) clear 
+   echo -e "#########################################################################"
+   echo "                   Printing User Information for this machine"
+   echo    "#########################################################################"
+   echo "  "
+   echo "  " 
+   xrandr --current 
+      echo  -e  " \n#########################################################################"
+   echo  -e  "\n Press Any key to Continue to main menu ..........................."
+   read -n 1 key;;
+*) echo "Invalid Selection .......Press Any Key to Continue . . . . . . . "
+esac
 done
 #echo -e "\n"
 #echo -e  "###############USERS LOGGED IN ##################"
-#who
+
 #echo -e "\n\n\n"
-#echo "#################CURRENTLY LOGGEND IN AS#########"
-#who am i
-#echo -e "\n\n"
-#echo "################# SYSTEM UPTIME################# "
-#uptime
-#echo -e "\n################CPU INFORMATION################ /c"
-#cat /proc/cpuinfo 
+
+
