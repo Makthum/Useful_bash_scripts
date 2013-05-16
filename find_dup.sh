a462c31ca69601736e0f03bab5404c8612750310
makthum <makthum@gmail.com>
2013-05-15 22:45:58 -0700

    Adding Find Duplicates

---
+++ find_dup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+clear
+echo "#####################################################################"
+echo "             Remove Duplicate File Tool                              "
+echo "####################################################################"
+
+echo -e "Enter the directory path from root to search \c"
+read search_path 
+echo -e "Enter where you want to store the log file \c"
+read log_path
+find $search_path/* -type f -exec md5sum {} \; | awk '{ print $1,"\t",$2 } ' | sort | uniq -d -w 32
