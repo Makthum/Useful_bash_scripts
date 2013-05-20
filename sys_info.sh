b55882883004d97fcd791467aae718655c3f5c39
Mohamed Makthum Mohamed <makthum@gmail.com>
2013-05-20 12:48:18 -0600

    Update sys_info.sh

--- sys_info.sh
+++ sys_info.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
-
+#Author Mohamed Makthum Mohamed 
 clear
 selection=2
+
+#welcome banner - is displayed only once when the tool in run for first time
+
 echo -e " ##########################################################################"
 echo -e " \n \t              welcome to System Information Tool \n "
 echo    " ##########################################################################"
+
+# Main Menu - Control is returned to Main Menu after every successful operation\
+# Pressing 9 at the Main menu will exit the program
+
 until [ $selection -eq "9" ]; do
 clear
 echo    " ##########################################################################"
@@ -21,19 +28,26 @@ echo -e "\n \t 	           Enter your Input : \c"
 
 read selection
 
+#Switch case operation is based only on Numerical Input  for other Input default message will be thrown 
+#After every operation text on the screen is cleared for better display 
+
 case $selection in
 
+# This option display all the users logged in the system  
 1) clear 
    echo -e "#########################################################################"
    echo "                   Printing Users Logged in on this machine"
    echo    "#########################################################################"
    echo "  "
    echo "  " 
-   who 
+   who -a
       echo  -e  " \n#########################################################################"
    echo  -e  "\n Press Any key to Continue to main menu ..........................."
    read -n 1 key
    ;;
+  
+# This option prints current user information and system uptime 
+
 2) clear 
    echo -e "#########################################################################"
    echo "                   Printing Login information & Uptime  for this machine"
@@ -50,6 +64,9 @@ case $selection in
    read -n 1 key
    ;;
 
+# This option prints the cpu information from proc files . If needed further information such as meminfo , etc can be 
+# included in the script
+
 3) clear 
    echo -e "#########################################################################"
    echo "                   Printing CPU Information for this machine"
@@ -62,6 +79,9 @@ case $selection in
    echo  -e  "\n Press Any key to Continue to main menu ..........................."
    read -n 1 key
    ;;
+   
+# This option prints display information along with the current resolution used .
+
 4) clear 
    echo -e "#########################################################################"
    echo "                   Printing Display resolution Information for this machine"
@@ -72,9 +92,12 @@ case $selection in
       echo  -e  " \n#########################################################################"
    echo  -e  "\n Press Any key to Continue to main menu ..........................."
    read -n 1 key;;
+   
+# Use this option to know OS info and Machine type : 32 or 64 bit machine 
+
 5) clear 
    echo -e "#########################################################################"
-   echo "                   Printing Display resolution Information for this machine"
+   echo "                   Printing Operating System Information for this machine"
    echo    "#########################################################################"
    echo "  "
    echo "  " 
@@ -83,6 +106,9 @@ case $selection in
       echo  -e  " \n#########################################################################"
    echo  -e  "\n Press Any key to Continue to main menu ..........................."
    read -n 1 key;;
+
+# This will print all the network interface information on the system . If needed it can be modififed using options 
+
 6) clear 
    echo -e "#########################################################################"
    echo "                   Printing Network Information for this machine"
@@ -96,9 +122,6 @@ case $selection in
 *) echo "Invalid Selection .......Press Any Key to Continue . . . . . . . "
 esac
 done
-#echo -e "\n"
-#echo -e  "###############USERS LOGGED IN ##################"
 
-#echo -e "\n\n\n"
 
 
